3500fa6551ea773a957ea09da50d21810c51aae7
James Lee <jameshonglee@google.com>
2023-08-31 16:01:04 -0700

    fix dupe imports

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -41,11 +41,6 @@ import {
   STATUS_STARTING,
   STATUS_SUCCESS
 } from './const';
-import pysparkLogo from '../../third_party/icons/pyspark_logo.svg';
-import pythonLogo from '../../third_party/icons/python_logo.svg';
-import sparkrLogo from '../../third_party/icons/sparkr_logo.svg';
-import scalaLogo from '../../third_party/icons/scala_logo.svg';
-import { LabIcon } from '@jupyterlab/ui-components';
 export interface IAuthCredentials {
   access_token?: string;
   project_id?: string;
