96b6d272bbcd2fe45cd2c3004cccab6a11b5b661
shanam-singla-accenture <130960454+shanam-singla-accenture@users.noreply.github.com>
2023-10-25 13:07:28 +0530

    0.1.53

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-export const VERSION_DETAIL = "0.1.52"
+export const VERSION_DETAIL = "0.1.53"
 export const CREATE_CLUSTER_URL =
   'https://console.cloud.google.com/dataproc/clusters';
 export const CREATE_BATCH_URL =
