324f0f4bfe1b87e97d6cb7b491373ac23bde9749
saranyaloganathan23 <130973008+saranyaloganathan23@users.noreply.github.com>
2024-01-03 12:42:48 +0530

    Dag list airflow uri changes

--- dataproc_jupyter_plugin/contollers/dagContoller.py
+++ dataproc_jupyter_plugin/contollers/dagContoller.py
@@ -7,5 +7,6 @@ class DagController(APIHandler):
     def get(self):
         dag = DagListService()
         credentials = handlers.get_cached_credentials(self.log)
-        dag_list = dag.list_jobs(credentials)
+        composer_name = 'composer4'
+        dag_list = dag.list_jobs(credentials,composer_name)
         self.finish(json.dumps(dag_list))
