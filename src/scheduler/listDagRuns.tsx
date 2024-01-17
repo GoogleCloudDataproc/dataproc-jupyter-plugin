5b49a93d574a83ba86d9d1ddfe3d6968d1c3db76
aditee-accenture <149396538+aditee-accenture@users.noreply.github.com>
2024-01-17 15:27:10 +0530

    State color change

--- src/scheduler/listDagRuns.tsx
+++ src/scheduler/listDagRuns.tsx
@@ -88,6 +88,46 @@ const ListDagRuns = ({
   );
 
   const tableDataCondition = (cell: ICellProps) => {
+    //add here
+    if (cell.column.Header === 'State') {
+      if (cell.value === 'success') {
+        return (
+          <td
+            {...cell.getCellProps()}
+            className="notebook-template-table-data-state-success"
+          >
+            {cell.render('Cell')}
+          </td>
+        );
+      } else if (cell.value === 'failed') {
+        return (
+          <td
+            {...cell.getCellProps()}
+            className="notebook-template-table-data-state-failure"
+          >
+            {cell.render('Cell')}
+          </td>
+        );
+      } else if (cell.value === 'running') {
+        return (
+          <td
+            {...cell.getCellProps()}
+            className="notebook-template-table-data-state-running"
+          >
+            {cell.render('Cell')}
+          </td>
+        );
+      } else if (cell.value === 'queued') {
+        return (
+          <td
+            {...cell.getCellProps()}
+            className="notebook-template-table-data-state-queued"
+          >
+            {cell.render('Cell')}
+          </td>
+        );
+      }
+    }
     return (
       <td {...cell.getCellProps()} className="notebook-template-table-data">
         {cell.render('Cell')}
