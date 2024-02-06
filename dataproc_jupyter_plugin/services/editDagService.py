4dd9e625b2fc1bc65e36be54d6b5d4d4e3944572
Jeyaprakash-NK <131133853+Jeyaprakash-NK@users.noreply.github.com>
2024-02-06 18:17:44 +0530

    UTC added as dummy value

--- dataproc_jupyter_plugin/services/editDagService.py
+++ dataproc_jupyter_plugin/services/editDagService.py
@@ -54,7 +54,7 @@ class DagEditService():
                     "email": ['test@gmail.com'],
                     "schedule_value": '* * * * *',
                     "stop_cluster": 'False',
-                    "time_zone": ''
+                    "time_zone": 'UTC'
                 }
                 print(payload)
                 return payload
