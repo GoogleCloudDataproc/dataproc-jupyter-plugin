42a55025acbb91a3b548b9a7fb2bda5ca0cb23aa
saranyaloganathan23 <130973008+saranyaloganathan23@users.noreply.github.com>
2024-02-07 13:00:52 +0530

    passing parameters

--- dataproc_jupyter_plugin/services/editDagService.py
+++ dataproc_jupyter_plugin/services/editDagService.py
@@ -28,6 +28,8 @@ class DagEditService():
             output, _ = process.communicate()
             cluster_name = ''
             serverless_name = ''
+            stop_cluster_check = 'False'
+            mode_selected = 'serverless'
             if process.returncode == 0:
                 log.info(f"Downloaded dag file")
                 downloads_path = os.path.expanduser('~/Downloads')
@@ -39,12 +41,28 @@ class DagEditService():
                             break
                 with open(file_path, 'r') as f:
                     for line in f:
+                        if 'parameters' in line:
+                            parameters = line.split('=')[-1].strip().strip("'\"")
+                            parameters_lines = [line.strip() for line in parameters.split('\n') if line.strip()]
+                            # Convert each line into a string in the format <key>:<value>
+                            parameters_list = [line for line in parameters_lines]
+                            break
+
+                with open(file_path, 'r') as f:
+                    for line in f:
+                        if 'email' in line:
+                            email_list = line.split(":")[-1].strip().strip("'\"").replace(',', '')
+                            # email_list = [email.strip().strip("'\"") for email in emails]  # Extract emails from the line
+                            print(email_list)
+                            break
+                with open(file_path, 'r') as f:
+                    for line in f:
                         if 'cluster_name' in line:
                             cluster_name = line.split(":")[-1].strip().strip("'\"}").split("'")[0].strip()# Extract project_id from the line
                             print('--------------------download and print--------------')
                             print(cluster_name)
                         elif 'submit_pyspark_job' in line:
-                            mode_selected = 'cluster' if 'submit_pyspark_job' in line else 'serverless'
+                            mode_selected = 'cluster' 
                             print(mode_selected)
                         elif "'retries'" in line:
                             retries = line.split(":")[-1].strip().strip("'\"},")
@@ -59,12 +77,22 @@ class DagEditService():
                             email_on_retry = second_part.split("'")[1]  
                         elif 'email_on_success' in line:
                             second_part = line.split(':')[1].strip()            
-                            email_on_success = second_part.split("'")[1]  
+                            email_on_success = second_part.split("'")[1]                 
+                        elif 'schedule_interval' in line:
+                            schedule_interval = line.split('=')[-1].strip().strip("'\"").split(',')[0].rstrip("'\"")  # Extract schedule_interval from the line
+                            print(schedule_interval)
+                        elif 'stop_cluster_check' in line:
+                            second_part = line.split(':')[1].strip()            
+                            stop_cluster_check = second_part.split("'")[1] 
+                        elif 'serverless_name' in line:
+                            serverless_name = line.split('=')[-1].strip().strip("'\"")
+                            break
+
                            
 
                 payload = {
                     "input_filename": input_notebook,
-                    "parameters":'[test: test1,test2:val]',  
+                    "parameters":parameters_list,  
                     "mode_selected": mode_selected,
                     "cluster_name":cluster_name,
                     "serverless_name":serverless_name,
@@ -73,9 +101,9 @@ class DagEditService():
                     "email_failure": email_on_failure,
                     "email_delay": email_on_retry,
                     "email_success": email_on_success,
-                    "email": ['test@gmail.com'],
-                    "schedule_value": '* * * * *',
-                    "stop_cluster": 'False',
+                    "email": email_list,
+                    "schedule_value": schedule_interval,
+                    "stop_cluster": stop_cluster_check,
                     "time_zone": 'UTC'
                 }
                 print(payload)
