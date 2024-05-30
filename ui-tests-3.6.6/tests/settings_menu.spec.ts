7daa528ee31e1edb3def3876f4c3dad7782f7639
Jeyaprakash-NK <131133853+Jeyaprakash-NK@users.noreply.github.com>
2024-05-30 10:09:39 +0530

    Settings name modified in test cases

--- ui-tests-3.6.6/tests/settings_menu.spec.ts
+++ ui-tests-3.6.6/tests/settings_menu.spec.ts
@@ -23,7 +23,7 @@ test.describe('Settings Menu', () => {
       .getByLabel('main', { exact: true })
       .getByText('Settings')
       .click();
-    await page.getByText('Google BigQuery Settings').click();
+    await page.getByText('Google Dataproc Settings').click();
   });
 
   test('Can change project', async ({ page }) => {
@@ -31,7 +31,7 @@ test.describe('Settings Menu', () => {
       .getByLabel('main', { exact: true })
       .getByText('Settings')
       .click();
-    await page.getByText('Google BigQuery Settings').click();
+    await page.getByText('Google Dataproc Settings').click();
 
     // Assert clearing the Project ID disables the save button.
     await page.getByRole('combobox', { name: 'Project ID' }).click();
