9944ff6ee8c9e06aac41f02f1e7daea3e4401931
aditee-accenture <aditee.abhijit.katti@accenture.com>
2024-07-30 17:32:29 +0530

    fix for gpu.amount

--- src/runtime/createRunTime.tsx
+++ src/runtime/createRunTime.tsx
@@ -241,8 +241,9 @@ function CreateRunTime({
   }, [networkSelected]);
 
   const modifyResourceAllocation = () => {
-    const regexPattern =  /^a100-(40|80)$/i;
+    const regexPattern = /^a100-(40|80)$/i;
     let resourceAllocationModify = [...resourceAllocationDetailUpdated];
+    let gpuDetailModify = [...GPU_DEFAULT];
     gpuDetailUpdated.forEach(item => {
       const [key, value] = item.split(':');
       if (key === 'spark.dataproc.executor.resource.accelerator.type') {
@@ -265,8 +266,20 @@ function CreateRunTime({
                 return 'spark.dataproc.executor.disk.size:750g';
               }
               if (item === 'spark.executor.cores:4') {
-                return 'spark.executor.cores:12';
+                const newCores = 12; // to change core value to 12 for a100-40 and a100-80
+                const gpuValue = (1 / newCores).toFixed(2); // Calculate the GPU value based on the new core count
+                gpuDetailModify = gpuDetailModify.map(gpuItem => {
+                  const [gpuKey] = gpuItem.split(':');
+                  if (gpuKey === 'spark.task.resource.gpu.amount') {
+                    return `spark.task.resource.gpu.amount:${gpuValue}`;
+                  }
+                  return gpuItem;
+                });
+
+                return `spark.executor.cores:${newCores}`;
               }
+              setGpuDetail(gpuDetailModify);
+              setGpuDetailUpdated(gpuDetailModify);
               return item;
             }
           );
@@ -277,12 +290,22 @@ function CreateRunTime({
                 return 'spark.dataproc.executor.disk.size:400g';
               }
               if (item === 'spark.executor.cores:12') {
-                return 'spark.executor.cores:4';
+                const newCores = 4; // reverting the value for spark.executor.cores for non a100 fields
+                const gpuValue = (1 / newCores).toFixed(2); // Calculate the GPU value based on the new core count
+                gpuDetailModify = gpuDetailModify.map(gpuItem => {
+                  const [gpuKey] = gpuItem.split(':');
+                  if (gpuKey === 'spark.task.resource.gpu.amount') {
+                    return `spark.task.resource.gpu.amount:${gpuValue}`;
+                  }
+                  return gpuItem;
+                });
+                setGpuDetail(gpuDetailModify);
+                setGpuDetailUpdated(gpuDetailModify);
+                return `spark.executor.cores:${newCores}`;
               }
               return item;
             }
           );
-
           if (
             !resourceAllocationModify.includes(
               'spark.dataproc.executor.disk.size:400g'
