f20f66280109e3d40e2b76f02dce9f919d502b25
Jeyaprakash-NK <jeyaprakash.nk@accenture.com>
2024-09-10 15:26:31 +0530

    session detail button disable added

--- src/controls/NotebookButtonExtension.tsx
+++ src/controls/NotebookButtonExtension.tsx
@@ -58,6 +58,7 @@ class NotebookButtonExtensionPoint implements IDisposable {
   isDisposed: boolean;
   private readonly sparkLogsButton: ToolbarButton;
   private readonly sessionDetailsButton: ToolbarButton;
+  private readonly sessionDetailsButtonDisable: ToolbarButton;
   private readonly notebookSchedulerButton: ToolbarButton;
   private sessionId?: string;
 
@@ -97,6 +98,18 @@ class NotebookButtonExtensionPoint implements IDisposable {
       1000,
       'session-details',
       this.sessionDetailsButton
+    );
+    this.sessionDetailsButton.hide();
+
+    this.sessionDetailsButtonDisable = new ToolbarButton({
+      icon: iconSessionLogs,
+      tooltip: 'Please wait till session is ready',
+      className: 'dark-theme-logs-disable'
+    });
+    this.panel.toolbar.insertItem(
+      1000,
+      'session-details-disable',
+      this.sessionDetailsButtonDisable
     );
 
     this.notebookSchedulerButton = new ToolbarButton({
@@ -157,8 +170,8 @@ class NotebookButtonExtensionPoint implements IDisposable {
    *  3) Show or hide the log and session details buttons as necessary.
    */
   private onKernelChanged = async (session: ISessionContext) => {
-    this.sparkLogsButton.hide();
     this.sessionDetailsButton.hide();
+
     // Get the current kernel ID and look for the kernel in the kernel API.
     const currentId = session.session?.kernel?.id;
     const runningKernels = await KernelAPI.listRunning();
@@ -173,6 +186,7 @@ class NotebookButtonExtensionPoint implements IDisposable {
       // hide everything and abort.
       this.sparkLogsButton.hide();
       this.sessionDetailsButton.hide();
+      this.sessionDetailsButtonDisable.hide();
       this.sessionId = undefined;
       this.shsUri = undefined;
       return;
@@ -190,12 +204,14 @@ class NotebookButtonExtensionPoint implements IDisposable {
       // TODO: fix this for Cluster Notebooks.
       this.sparkLogsButton.hide();
       this.sessionDetailsButton.hide();
+      this.sessionDetailsButtonDisable.hide();
       this.sessionId = undefined;
       this.shsUri = undefined;
       return;
     }
 
     // If session ID is specified, show session detail button.
+    this.sessionDetailsButtonDisable.hide();
     this.sessionDetailsButton.show();
 
     // Fetch session details (we care about the SHS URL) from OnePlatform.
