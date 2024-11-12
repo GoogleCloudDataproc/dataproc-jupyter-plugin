63b038ee8d158878a06e365b96fa8f4ca6fae2e1
risha-vijayvargiya-22 <risha.vijayvargiya@accenture.com>
2024-11-12 12:57:03 +0530

    UI config api changes

--- dataproc_jupyter_plugin/handlers.py
+++ dataproc_jupyter_plugin/handlers.py
@@ -38,6 +38,7 @@ from dataproc_jupyter_plugin.controllers import (
     dataproc,
     executor,
 )
+from dataproc_jupyter_plugin.controllers.vertex import vertex
 
 _region_not_set_error = """GCP region not set in gcloud.
 
@@ -209,6 +210,7 @@ def setup_handlers(web_app):
         "bigQueryPreview": bigquery.PreviewController,
         "bigQueryProjectsList": bigquery.ProjectsController,
         "bigQuerySearch": bigquery.SearchController,
+        "api/vertex/uiConfig": vertex.UIConfigController
     }
     handlers = [(full_path(name), handler) for name, handler in handlersMap.items()]
     web_app.add_handlers(host_pattern, handlers)
