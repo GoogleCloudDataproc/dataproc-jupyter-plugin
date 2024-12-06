c6a789ccc9dc36eb59f4548a9df58986c8d2c6be
saranyaloganathan23 <saranya.lognathan@accenture.com>
2024-12-06 11:14:56 +0530

    reverted init.py

--- dataproc_jupyter_plugin/__init__.py
+++ dataproc_jupyter_plugin/__init__.py
@@ -12,15 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.from ._version import __version__
 import logging
-import sys
+
 
 from google.cloud.jupyter_config.tokenrenewer import CommandTokenRenewer
 from jupyter_server.services.sessions.sessionmanager import SessionManager
 from kernels_mixer.kernels import MixingMappingKernelManager
 from kernels_mixer.kernelspecs import MixingKernelSpecManager
 from kernels_mixer.websockets import DelegatingWebsocketConnection
-from traitlets import Bool
-from jupyter_server.serverapp import ServerApp
+
 
 
 from .handlers import DataprocPluginConfig, configure_gateway_client_url, setup_handlers
