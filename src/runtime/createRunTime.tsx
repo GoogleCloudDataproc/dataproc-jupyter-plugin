9b7ff9a9e2ecb3a255511806c59898630ee7217c
saranyaloganathan23 <saranya.lognathan@accenture.com>
2025-03-17 18:37:52 +0530

    Update createRunTime.tsx

--- src/runtime/createRunTime.tsx
+++ src/runtime/createRunTime.tsx
@@ -1091,12 +1091,11 @@ function CreateRunTime({
               autoTimeSelected && {
                 ttl: autoTimeSelected + 's'
               }),
-            ...(serviceAccountSelected !== '' &&
-              selectedAccountRadio === 'userAccount' && {
-                authentication_config: {
-                  user_workload_authentication_type: 'END_USER_CREDENTIALS'
-                }
-              })
+            ...(selectedAccountRadio === 'userAccount' && {
+              authentication_config: {
+                user_workload_authentication_type: 'END_USER_CREDENTIALS'
+              }
+            })
           },
           peripheralsConfig: {
             ...(servicesSelected !== 'None' && {
