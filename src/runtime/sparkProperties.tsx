441e49449259d02abfa83e807224564dd3af87a6
srinivasan-acn <srinivasan.a.ganesan@accenture.com>
2026-07-08 05:05:41 +0000

    resolved uncontrolled input and layout regression in SparkProperties

--- src/runtime/sparkProperties.tsx
+++ src/runtime/sparkProperties.tsx
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -267,13 +267,16 @@ function SparkProperties({
                               'spark.dataproc.executor.compute.tier' ||
                             (sparkSection === 'metastore' && index === 2)
                           }
-                          placeholder={
+                          value={
                             labelDetailUpdated[index]
                               ? labelDetailUpdated[index].substring(
                                   labelDetailUpdated[index].indexOf(':') + 1
                                 )
                               : ''
                           }
+                          placeholder={labelSplit[1]}
+                          Label={`Value ${index + 1}`}
+                          InputLabelProps={{ shrink: true }}
                         />
                       )}
                     </div>
